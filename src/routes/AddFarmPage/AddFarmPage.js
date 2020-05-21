be9545c750c8211e314f239225063d2a0587bd30
Rise Erpelding <rise.erpelding@gmail.com>
2020-05-21 18:43:50 -0500

    adding AddFarmPage and NotFoundPage components

---
+++ src/routes/AddFarmPage/AddFarmPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AddFarmPage extends Component {
+  render() {
+    return (
+      <div>This is the AddFarmPage</div>
+    )
+  }
+}
+
+export default AddFarmPage
---
+++ src/routes/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>This is the NotFoundPage</div>
+    )
+  }
+}
+
+export default NotFoundPage
