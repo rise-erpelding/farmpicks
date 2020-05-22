c46f96feaac88094ecb68a27e72d29c8163469f5
Rise Erpelding <rise.erpelding@gmail.com>
2020-05-21 20:14:28 -0500

    styling NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Harvest from '../Logo/Harvest.png'
+import './NavBar.css'
 
 class NavBar extends Component {
   render () {
     return (
-      <div>
-        <Link to ='/'>O FarmPicks</Link>
+      <div className="nav-bar"> 
+        <Link to ='/'>
+          <img 
+            className="nav-bar__logo" 
+            src={Harvest} 
+            alt="FarmPicks Logo" />
+          {' '}FarmPicks
+        </Link>
       </div>
     )
   }
