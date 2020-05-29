fbbea2d4f6785443031b1a9503533eac89244630
Rise Erpelding <rise.erpelding@gmail.com>
2020-05-28 20:51:13 -0500

    styling landing page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import Harvest from '../Logo/Harvest.png'
 import './NavBar.css'
 
 class NavBar extends Component {
   render () {
     return (
-      <div className="nav-bar"> 
-        <Link to ='/'>
-          <img 
-            className="nav-bar__logo" 
-            src={Harvest} 
-            alt="FarmPicks Logo" />
-          {' '}FarmPicks
-        </Link>
+      <div className="nav-bar">
+        <div className="nav-bar__sign-in">Sign in</div>
+        <div className="nav-bar__home">
+          <Link to ='/'>FarmPicks</Link>
+          <p>From small farms to you.</p>
+        </div>
+
       </div>
     )
   }
