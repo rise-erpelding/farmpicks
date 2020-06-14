690873548b8dc12316ddb05bf5ff5274fa8da8df
Rise <rise.erpelding@gmail.com>
2020-06-14 16:22:49 -0500

    updated test to add icons

--- src/routes/RegistrationPage/RegistrationPage.test.js
+++ src/routes/RegistrationPage/RegistrationPage.test.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RegistrationPage from './RegistrationPage';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RegistrationPage from './RegistrationPage'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faHeart as faHeartSolid, faChevronLeft, faCaretDown, faFilter, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+
+library.add(
+  faSearch,
+  faHeartSolid,
+  faHeartRegular,
+  faChevronLeft,
+  faCaretDown,
+  faFilter,
+  faUser
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
