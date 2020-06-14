aff47319b5b42eaf336ad228bb864a6d6a6ffe91
Rise <rise.erpelding@gmail.com>
2020-06-14 15:56:00 -0500

    removed email form from RegistrationPage

--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -17,23 +17,6 @@ class RegistrationPage extends Component {
     return (
       <div className='registration-page'>
         <h3>Registration coming soon!</h3>
-        <p>Sign up to be the first to know when registration opens.</p>
-        <form 
-          className='registration-page__form'
-          onSubmit={this.handleSubmit}>
-          <div className='registration-page__email'>
-            <label htmlFor='email'>Email</label>
-            {' '}
-            <input 
-              required
-              name='email'
-              type='email'
-              id='email'
-              />
-          </div>
-          <button type='button' onClick={this.handleClickCancel}>Cancel</button>
-          <button type='submit'>Register</button>
-        </form>
         <DemoLoginInfo />
       </div>
     )
