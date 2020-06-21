ab570bb537600f1078a99fee1e85dae998b8b977
Rise <rise.erpelding@gmail.com>
2020-06-20 22:16:15 -0500

    applying airbnb react style guide

--- src/routes/MyProfilePage/MyProfilePage.js
+++ src/routes/MyProfilePage/MyProfilePage.js
@@ -1,100 +1,123 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import FarmListItem from '../../components/FarmListItem/FarmListItem'
-import FarmsApiService from '../../services/farms-api-service'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './MyProfilePage.css'
-
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FarmListItem from '../../components/FarmListItem/FarmListItem';
+import FarmsApiService from '../../services/farms-api-service';
+import './MyProfilePage.css';
 
 class MyProfilePage extends Component {
-
-  state = {
-    firstName: '',
-    lastName: '',
-    userName: '',
-    userType: '',
-    userId: '',
-    favoriteFarms: [],
-    error: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      userType: '',
+      userId: '',
+      favoriteFarms: [],
+      // eslint-disable-next-line react/no-unused-state
+      error: null,
+    };
   }
 
   componentDidMount() {
     FarmsApiService.getUserInfo()
-      .then(response => {
+      .then((response) => {
         this.setState({
           firstName: response[0].first_name,
           lastName: response[0].last_name,
           userName: response[0].user_name,
           userType: response[0].user_type,
           userId: response[0].id,
-        })
+        });
       })
-      .catch(error => {
-        console.error(error)
-        this.setState({ error })
-      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        // eslint-disable-next-line react/no-unused-state
+        this.setState({ error });
+      });
     FarmsApiService.getUserFavorites()
-      .then(response => {
+      .then((response) => {
         this.setState({
-          favoriteFarms: response
-        })
+          favoriteFarms: response,
+        });
       })
-      .catch(error => {
-        console.error(error)
-        this.setState({ error })
-      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        // eslint-disable-next-line react/no-unused-state
+        this.setState({ error });
+      });
   }
 
-  render () {
+  render() {
+    const {
+      firstName, lastName, userName, userType, favoriteFarms, userId,
+    } = this.state;
 
-    const { firstName, lastName, userName, userType } = this.state
-
-    let myInfo
-
-    if (this.state.userType === 'Consumer') {
-      myInfo = 
+    /* This logic would potentially change how profile page renders based on whether user is a
+    consumer or a farmer. Functionality for farmer page hasn't been fully built out yet on backend
+    --ideally a farmer column would be added to the farms table to link user id with farm. For now,
+    since seed data happens to be structured so that farms id column and users id column match,
+    this is a workaround. */
+    let myInfo;
+    if (userType === 'Consumer') {
+      myInfo = (
         <>
           <h3>My Saved Farms:</h3>
-          <ul className='my-profile-page__farms-list'>
-            {this.state.favoriteFarms.map(farm =>
+          <ul className="my-profile-page__farms-list">
+            {favoriteFarms.map((farm) => (
               <li key={farm.id}>
                 <FarmListItem info={farm} />
-              </li>)}
+              </li>
+            ))}
           </ul>
         </>
-    } else if (this.state.userType === 'Farmer') {
-      myInfo =
-        <Link to={`/farms/${this.state.userId}`}>
+      );
+    } else if (userType === 'Farmer') {
+      myInfo = (
+        <Link to={`/farms/${userId}`}>
           My Farm Page
         </Link>
+      );
     }
 
-
     return (
-      <main className='my-profile-page'>
-        <Link to='/'>
-          <FontAwesomeIcon 
-            icon='chevron-left'
-            className='my-profile-page__back'
+      <main className="my-profile-page">
+        <Link to="/">
+          <FontAwesomeIcon
+            icon="chevron-left"
+            className="my-profile-page__back"
           />
         </Link>
-        <div className='my-profile-page__profile-info'>
+        <div className="my-profile-page__profile-info">
           <h2>Profile information</h2>
-          <div className='my-profile-page__avatar'>
+          <div className="my-profile-page__avatar">
             <FontAwesomeIcon
-              className='my-profile-page__avatar--graphic'
-              icon='user' 
+              className="my-profile-page__avatar--graphic"
+              icon="user"
             />
           </div>
-          <h3>{firstName}{' '}{lastName}</h3>
-          <h6>{userType} | username: {userName}</h6>
+          <h3>
+            {firstName}
+            {' '}
+            {lastName}
+          </h3>
+          <h6>
+            {userType}
+            {' '}
+            | username:
+            {' '}
+            {userName}
+          </h6>
         </div>
-        <div className='my-profile-page__farms'>
+        <div className="my-profile-page__farms">
           {myInfo}
         </div>
       </main>
-    )
+    );
   }
 }
 
-export default MyProfilePage
+export default MyProfilePage;
